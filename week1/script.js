85f9789549e67fcd74ef63724b5a12e8f774e31b
Kristen Ingelman <kristeningelman@hotmail.com>
2020-11-28 11:56:45 -0800

    week 1 review

---
+++ week1/script.js
@@ -0,0 +1,10 @@
+// Objects
+var animal = {
+  name: "bee",
+  description: "flies",
+  numberOfLegs: 6
+}
+
+animal.description = "yellow and black";
+console.log(animal.description);
+console.log(animal['numberOfLegs']);
