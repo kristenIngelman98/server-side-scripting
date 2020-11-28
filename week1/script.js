b470f8e10c3b3868168cd56fe7f5e910c6fe86d6
Kristen Ingelman <kristeningelman@hotmail.com>
2020-11-28 12:10:57 -0800

    added script.js

--- week1/script.js
+++ week1/script.js
@@ -8,3 +8,30 @@ var animal = {
 animal.description = "yellow and black";
 console.log(animal.description);
 console.log(animal['numberOfLegs']);
+
+// Arrays
+var animals = ['whale', 'llama', 'duck'];
+
+console.log(animals[2]); // outputs duck
+
+animals.push('wildabeast');
+console.log(animals[3]);
+
+// Map - transforms one array into another array
+var animalIntroductions = animals.map(function(an){
+  return "Hi " + an + ", nice to meet you!";
+});
+
+console.log(animalIntroductions);
+
+// ES6
+for (var i=0; i<10; i++) {
+  console.log("count i: " + i);
+}
+console.log(i);
+
+// Using let
+for (let j=0; j<10; j++) {
+  console.log("count j: " + j);
+}
+console.log(j);
