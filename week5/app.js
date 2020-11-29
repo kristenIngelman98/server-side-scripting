9d375c25787397e67f63628b5f86b5553e04adeb
Kristen Ingelman <kristeningelman@hotmail.com>
2020-11-29 12:13:15 -0800

    completed week5 exercises

--- week5/app.js
+++ week5/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
-
+app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+  res.render("index",  {animals:["red panda", "hyena", "dog", "cat"]});
+});
+
 
 const server = app.listen(process.env.PORT || 8080,
   () => console.log("listening"));
