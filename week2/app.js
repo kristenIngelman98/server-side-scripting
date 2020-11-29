7544b488562281318d6ba43585116e38793a1bc3
Kristen Ingelman <kristeningelman@hotmail.com>
2020-11-28 16:11:36 -0800

    completed week2 exercises

--- week2/app.js
+++ week2/app.js
@@ -28,3 +28,36 @@ app.get('/:something', (req,res) => {
   res.send(`You're trying to find ${something}?`);
 });
 
+const data = {
+  animals: [
+    {
+      name: "bee",
+      description: "flying insect"
+    },
+    {
+      name: "whale",
+      description: "large sea creature"
+    },
+    {
+      name: "duck",
+      description: "swim in ponds"
+    }
+  ]
+};
+
+app.get('/animals/:animalName', (req, res) => {
+  let animalName = req.params.animalName;
+
+  // send a description of the requested animal from the data object
+  // if it doesnt exist, send an error message
+  // use the array find method
+
+  let animal = data.animals.find(an => an.name == animalName);
+
+  if (animal != undefined) {
+    res.send(animal.description); // default status is 200 "ok"
+  }
+  else {
+    res.status(404).send("No animal with that name found!"); // 404 means "not found"
+  }
+});
