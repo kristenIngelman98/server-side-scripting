fbed27859b6040782c3681364a013184508b6699
Kristen Ingelman <kristeningelman@hotmail.com>
2020-11-28 18:28:19 -0800

    finished week3 exercises about middleware

--- week3/app.js
+++ week3/app.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const app = express();
 
+// Static middleware allows us to easily serve static files
+// Order of middleware matters
+app.use(express.static("public"));
+
+
+app.get('/', (req,res) => {
+  res.send("<h1>Not Using Middleware</h1>");
+})
+
+const ourFirstMiddleware = (req,res,next) => {
+
+  res.locals.message = "this message is stored in a  variable that is scoped to the current request-response cycle.";
+
+  next(); // remember to do this or the application will hang
+}
+
+app.use('/a', ourFirstMiddleware);
+
+app.get('/a', (req,res) => {
+  res.send("a: " + res.locals.message);
+});
+
+app.get('/b', (req,res) => {
+  res.send("b: " + res.locals.message);
+});
+
+
 const server = app.listen(process.env.PORT || 8080,
   () => console.log("listening"));
