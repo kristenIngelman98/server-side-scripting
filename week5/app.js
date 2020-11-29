9403e7de265f71264938a32e5dfc7c5dc72ba0bc
Kristen Ingelman <kristeningelman@hotmail.com>
2020-11-28 20:33:12 -0800

    installed ejs module

--- week5/app.js
+++ week5/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
 
+
+app.set("view engine", "ejs");
+
 const server = app.listen(process.env.PORT || 8080,
   () => console.log("listening"));
